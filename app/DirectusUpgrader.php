0d9d2b4aa367ee2698cf47ad154fcb2247da3c4e
Korneel Eeckhout <korneel@pienter.tech>
2019-09-24 16:03:15 +0200

    [feature] - Add upgradescript

--- app/DirectusUpgrader.php
+++ app/DirectusUpgrader.php
@@ -13,6 +13,8 @@ class DirectusUpgrader
     private $cmd;
     /** @var bool */
     public $verbose = false;
+    /** @var bool */
+    public $git = false;
     /** @var string */
     public $root;
 
@@ -21,17 +23,91 @@ class DirectusUpgrader
         $this->setOptions();
         $this->verbose = $this->cmd['verbose'];
         $this->root = $this->cmd['root'];
+        $this->git = $this->cmd['git'];
     }
 
-    public function echo($string, $type = 'normal') {
+    public function run() {
+        $this->repoClean();
+        $this->clone();
+        $this->cloneClean();
+        $this->backupComposer();
+        $this->moveNewDirectus();
+        $this->repoClean();
+        $this->addToGit();
+        $this->info();
+    }
+
+    private function repoClean() {
+        if (is_file($this->root . '/composer.bckp.json')) {
+            $this->log('Delete composer.bckp.json');
+            unlink($this->root . '/composer.bckp.json');
+        }
+
+        if (is_dir($this->root . '/upgrade_directus')) {
+            $this->log('Delete upgrade_directus folder');
+            system('rm -rf ' . $this->root . '/upgrade_directus/');
+        }
+    }
+
+    private function clone() {
+        $this->log('Clone repo');
+        $quiet = $this->verbose ? '' : '-q';
+        system("git clone {$quiet} https://github.com/directus/directus.git {$this->root}/upgrade_directus");
+//        $tags = [];
+//        exec('git ls-remote --tags https://github.com/directus/directus.git', $tags);
+//        print_r($tags);
+    }
+
+    private function cloneClean() {
+        system('rm -rf ' . $this->root . '/upgrade_directus/.git/');
+        system('rm -rf ' . $this->root . '/upgrade_directus/.github/');
+        system('rm -rf ' . $this->root . '/upgrade_directus/public/uploads/');
+        system('rm -rf ' . $this->root . '/upgrade_directus/public/extensions/custom/');
+        system('rm -rf ' . $this->root . '/upgrade_directus/logs/');
+        system('rm -rf ' . $this->root . '/upgrade_directus/vendor/');
+        system('rm ' . $this->root . '/upgrade_directus/public/admin/config.js');
+        system('rm ' . $this->root . '/upgrade_directus/public/admin/style.css');
+        system('rm ' . $this->root . '/upgrade_directus/public/admin/script.js');
+        system('rm ' . $this->root . '/upgrade_directus/.gitignore');
+        system('rm ' . $this->root . '/upgrade_directus/LICENSE.md');
+        system('rm ' . $this->root . '/upgrade_directus/README.md');
+    }
+
+    private function backupComposer() {
+        system("mv {$this->root}/composer.json {$this->root}/composer.bckp.json");
+    }
+
+    private function moveNewDirectus() {
+        system("cp -r {$this->root}/upgrade_directus/ {$this->root}/");
+    }
+
+    private function addToGit() {
+        if($this->git) {
+            system("git --git-dir {$this->root}/.git add .");
+            $this->log('Added new files to git');
+        }
+    }
+
+    public function log($string, $type = 'normal') {
         switch ($type) {
             case 'normal':
-                echo Color::white() . $string . Color::reset() . PHP_EOL;
+                if($this->verbose){
+                    echo Color::white() . $string . Color::reset() . PHP_EOL;
+                }
                 break;
             case 'alert':
                 echo Color::red() . $string . Color::reset() . PHP_EOL;
                 break;
+            case 'info':
+                echo Color::bold_blue() . $string . Color::reset() . PHP_EOL;
+                break;
         }
+    }
+
+    public function info() {
+        $this->log('Compare composer.json and composer.bckp.json', 'info');
+        $this->log('then run composer update', 'info');
+
     }
 
     private function setOptions() {
@@ -42,6 +118,7 @@ class DirectusUpgrader
 
         $this->cmd->option('r')
             ->aka('root')
+            ->describedAs('Set root (if empty working directory will be used')
             ->map(function($value){
                 if ($value) {
                     return rtrim($value, '/');
@@ -49,5 +126,10 @@ class DirectusUpgrader
                     return getcwd();
                 }
             });
+
+        $this->cmd->option('g')
+            ->aka('git')
+            ->describedAs('Add new files to git')
+            ->boolean();
     }
 }
